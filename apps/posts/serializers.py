301ea043205381c76c2a9ad52f5841ff6e820173
Nazymok Dmitriy <nazymok.d.lightit@gmail.com>
2021-09-29 10:45:22 +0300

    remove UserSerializer

--- apps/posts/serializers.py
+++ apps/posts/serializers.py
@@ -3,15 +3,6 @@ from django.contrib.auth.models import User
 from .models import Post
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    email = serializers.EmailField()
-    username = serializers.CharField(max_length=100)
-
-    class Meta:
-        model = User
-        fields = ['email', 'username']
-
-
 class PostSerializer(serializers.HyperlinkedModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
 
