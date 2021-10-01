205327fd0a1f0942f51fa312bad4755732b6d32f
Nazymok Dmitriy <nazymok.d.lightit@gmail.com>
2021-10-01 15:33:04 +0300

    add test case for users

--- apps/users/tests/test_views.py
+++ apps/users/tests/test_views.py
@@ -4,13 +4,22 @@ from rest_framework.test import APITestCase
 
 
 class CreateTest(APITestCase):
-    user_data = {
+    valid_data = {
         'username': 'username',
         'email': 'email@gmail.com',
         'password': 'password'
     }
+    invalid_data = {
+        'username': 'username',
+        'email': 'email',
+        'password': 'password'
+    }
 
     def test_can_create_user(self):
-        response = self.client.post(reverse('users:index'), self.user_data)
+        response = self.client.post(reverse('users:index'), self.valid_data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['username'], 'username')
+
+    def test_cant_create_user_with_invalid_data(self):
+        response = self.client.post(reverse('users:index'), self.invalid_data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
