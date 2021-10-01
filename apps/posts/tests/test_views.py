4ad52a0c3bd937d6c55ad9e4f3b1e9558c123ece
Nazymok Dmitriy <nazymok.d.lightit@gmail.com>
2021-10-01 11:08:25 +0300

    add test case

--- apps/posts/tests/test_views.py
+++ apps/posts/tests/test_views.py
@@ -14,6 +14,11 @@ class ListCreateViewTest(TestSetUp):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['author'], 'username')
 
+    def test_cant_create_post_with_no_data(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(reverse('posts:index'))
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
     def test_unauthorized_user_cant_create_post(self):
         response = self.client.post(reverse('posts:index'), self.post_data)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
