64c33875c3fad418796759daee09163f7dedecaf
Nazymok Dmitriy <nazymok.d.lightit@gmail.com>
2021-10-07 10:11:04 +0300

    add count_of_likes field for Post

--- apps/posts/serializers.py
+++ apps/posts/serializers.py
@@ -4,11 +4,15 @@ from .models import Post, Like
 
 class PostSerializer(serializers.HyperlinkedModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
+    count_of_likes = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Post
         fields = ['id', 'title', 'description', 'content', 'date_posted',
-                  'author']
+                  'author', 'count_of_likes']
+
+    def get_count_of_likes(self, obj):
+        return obj.likes.count()
 
 
 class LikeSerializer(serializers.HyperlinkedModelSerializer):
